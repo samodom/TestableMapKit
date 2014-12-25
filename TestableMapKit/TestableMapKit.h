bea11df4e0607024e064f7827267d42afa47c74a
Sam Odom <samodom@me.com>
2014-12-24 23:33:47 -0600

    Initial Commit

---
+++ TestableMapKit/TestableMapKit.h
@@ -0,0 +1,19 @@
+//
+//  TestableMapKit.h
+//  TestableMapKit
+//
+//  Created by Sam Odom on 12/24/14.
+//  Copyright (c) 2014 Swagger Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestableMapKit.
+FOUNDATION_EXPORT double TestableMapKitVersionNumber;
+
+//! Project version string for TestableMapKit.
+FOUNDATION_EXPORT const unsigned char TestableMapKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestableMapKit/PublicHeader.h>
+
+
